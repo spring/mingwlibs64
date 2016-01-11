664c487827f97eaa6f67de98d5dbb6ffca3e6df3
Eshed <ashdnazg@gmail.com>
2016-01-11 23:00:31 +0200

    Add mingwlibs64

---
+++ README.markdown
@@ -0,0 +1,161 @@
+# MinGW libraries for the Spring RTS game engine - 64 bit
+
+## README
+
+The files in this package are needed to compile spring with MinGW.
+WARNING - 64 bit windows builds of spring are experimental at best
+and will not sync with other builds.
+
+
+### Notes for users
+
+There are two ways of using/setting-up mingwlibs.
+
+1. The simple one (recommended for new users)
+
+Have them in a folder named mingwlibs where you have extracted
+the spring source tarball or git repository. This way,
+SCons or CMake will automatically find the libs.
+Your directory structure should look like this:
+
+	AI/                 Spring AI source code
+	game/               Output binaries go here
+	installer/          Scripts to build installer
+	rts/                Spring source code
+	mingwlibs/          This package!
+	mingwlibs/bin/      Executables
+	mingwlibs/dll/      Dynamic link libraries
+	mingwlibs/include/  Header files
+	mingwlibs/jlib/     Java libraries
+	mingwlibs/lib/      Native import libraries
+
+
+2. For multiple spring repositories
+
+This is useful  if you have multiple spring repositories,
+and you do not want to maintain mingwlibs separately for each of them,
+or if you simple want to keep it away from the spring sources.
+
+Just extract the archive anywhere you want it, eg like this:
+
+	C:\mingwlibs\bin
+	C:\mingwlibs\dll
+	C:\mingwlibs\include
+	C:\mingwlibs\jlib
+	C:\mingwlibs\lib
+
+For the spring build-system to find the libs, you have to specify them
+in the configure step like this:
+
+_CMake_
+
+	cmake -DMINGWLIBS=C:\mingwlibs [other-options...]
+
+
+### Notes for maintainers
+
+#### Creating MinGW32 import libraries
+
+Also included is `reimp_new.zip`, this contains `reimp.exe` which has been
+used to create MinGW32 import libraries for `DevIL.dll`, `ILU.dll` and `ILUT.dll`:
+
+	reimp DevIL.dll
+	reimp ILU.dll
+	reimp ILUT.dll
+
+This creates `libdevil.a`, `libilu.a`, `libilut.a` and a number of `.def` files.
+You can throw away the `.def` files and move the `lib*.a` files to `lib/`.
+
+#### Cross-Compiling Boost
+
+Download [the latest boost sources archive](http://www.boost.org/users/download/).
+
+Due to the nature of boost, it is very likely these steps will have to be adapted
+in-between versions.
+
+_Adjust_ and run boost_crosscompile.sh.
+
+You should now have both the static libs and the headers of the new boost
+version in your mingwlibs dir, and are only left to do the git magic to commit,
+and optionally mention the new version in the list below.
+
+### Current versions of included libraries and binaries
+
+* GNU __AWK__ 3.1.6
+
+	<http://home.claranet.de/xyzzy/home/ltru/gawk-win.htm>
+
+	This version does not depend on any dlls, and does not segfault
+	like the official GNU `awk.exe`.
+
+* __7-Zip__ (A) 4.65 2009-02-03
+
+	<http://www.7-zip.org>
+
+* __Boost__ (see boost_crosscompile.sh for the used version)
+
+	<http://www.boost.org>
+
+* __Curl__ 7.24.0
+	<http://curl.haxx.se/>
+
+* __Devil__ 1.7.8
+
+	<http://prdownloads.sourceforge.net/openil/Devil-1.6.8-rc2-win-.zip?download>
+
+* __FreeType__ 2.3.5
+
+	<http://gnuwin32.sourceforge.net/downlinks/freetype-bin-zip.php>
+
+	<http://gnuwin32.sourceforge.net/downlinks/freetype-lib-zip.php>
+
+	(Index: <http://gnuwin32.sourceforge.net/packages/freetype.htm>)
+
+	Note: since v9 downgraded to 2.1.10.2079 again because the particular 2.3.5 that
+	was included showed small misaligned fonts (works fine with 2.3.5 on linux.)
+	Do not know where I originally downloaded 2.1.10.2079
+
+* __GLEW__ 1.5.8.0
+
+	<http://glew.sourceforge.net>
+
+* __SDL__ 2.0.3
+
+	<http://libsdl.org/release/SDL2-devel-2.0.3-mingw.tar.gz>
+
+* __Java__ Development Kit SE 1.6.0 update 10 __headers__ and __import library__:
+
+	<http://java.sun.com/javase/downloads/index.jsp>
+
+	<http://www.inonit.com/cygwin/jni/invocationApi/execute.html>
+
+* __pandoc__ 1.8.1.2
+
+	<http://johnmacfarlane.net/pandoc/>
+
+* __Zlib__ 1.2.7
+
+	compiled with https://github.com/abma/pr-downloader/blob/73acf328d6f9b68a9b9ffe33eb2db3c1a7ab45df/scripts/crosscompile.sh
+
+* __OpenAL Soft__ 1.15.1
+
+	<http://kcat.strangesoft.net/openal.html>
+
+	irc.freenode.net:6667 #OpenAL
+
+* __Ogg/Vorbis/Vorbisfile__
+
+	`libogg-1.1.4`, `libvorbis-1.2.3`
+
+	compiled with mingw gcc 4.4.1
+
+* Java __Vecmath__ (from Java 3D 1.5)
+
+	<http://vecmath.dev.java.net>
+
+* MS C Run-Time Libraries __MSVCR71.dll__
+
+	<http://msdn.microsoft.com/en-us/library/abx4dbyh(VS.71).aspx>
+
+	on that page, see:
+		_What is the difference between msvcrt.dll and msvcr71.dll?_
---
+++ buildbot/mingwlibs_boost.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+set -e
+
+# Setting system dependent vars
+BOOST_DIR=/tmp/boost/
+BOOST_BUILD_DIR=/tmp/build-boost/
+MINGWLIBS_DIR=/tmp/mingwlibs/
+BOOST_MAJOR=1
+BOOST_MINOR=55
+BOOST_PATCH=0
+BOOST_FILE=boost_${BOOST_MAJOR}_${BOOST_MINOR}_${BOOST_PATCH}.tar.bz2?download
+BOOST_DL=http://sourceforge.net/projects/boost/files/boost/${BOOST_MAJOR}.${BOOST_MINOR}.${BOOST_PATCH}/$BOOST_FILE
+
+# spring's boost dependencies
+BOOST_LIBS="test thread system regex filesystem program_options signals chrono"
+SPRING_HEADERS="${BOOST_LIBS} format ptr_container spirit algorithm date_time asio signals2"
+ASSIMP_HEADERS="math/common_factor smart_ptr"
+BOOST_HEADERS="${SPRING_HEADERS} ${ASSIMP_HEADERS}"
+BOOST_CONF=${BOOST_BUILD_DIR}/user-config.jam
+
+set +e
+## disable fetch by emerge as it requires access to the portage group (which buildbot doesn't have)
+# EMERGE=$(which emerge)
+set -e
+
+# x86 or x86_64
+MINGW_GPP=/usr/bin/i686-w64-mingw32-g++
+GCC_VERSION=$(${MINGW_GPP} -dumpversion)
+
+BOOST_LIBS_ARG=""
+for LIB in $BOOST_LIBS
+do
+	BOOST_LIBS_ARG="${BOOST_LIBS_ARG} --with-${LIB}"
+done
+
+
+#############################################################################################################
+
+# cleanup
+echo -e "\n---------------------------------------------------"
+echo "-- clear"
+rm -rf ${BOOST_BUILD_DIR}
+rm -rf ${BOOST_DIR}
+rm -rf ${MINGWLIBS_DIR}
+
+
+# git clone mingwlibs repo (needed for later uploading)
+echo -e "\n---------------------------------------------------"
+echo "-- clone git repo"
+git clone -l -s -n .  ${MINGWLIBS_DIR}
+cd ${MINGWLIBS_DIR}
+git fetch
+git checkout -f master
+
+
+# Setup final structure
+echo -e "\n---------------------------------------------------"
+echo "-- setup dirs"
+mkdir -p ${BOOST_DIR}
+rm -f ${MINGWLIBS_DIR}lib/libboost*
+rm -rf ${MINGWLIBS_DIR}include/boost
+mkdir -p ${MINGWLIBS_DIR}lib/
+mkdir -p ${MINGWLIBS_DIR}include/boost/
+
+
+# Gentoo related - retrieve boost's tarball
+echo -e "\n---------------------------------------------------"
+echo "-- fetching boost's tarball"
+
+if [ -n "$EMERGE" ] && [ -x "$EMERGE" ]; then
+	$EMERGE boost --fetchonly &>/dev/null
+	source /etc/portage/make.conf
+	BOOST_FILE=$(find ${DISTDIR} -iname "boost_*.tar.*" -print -quit)
+else
+	wget -P /tmp -N --no-verbose ${BOOST_DL}
+	BOOST_FILE="/tmp/${BOOST_FILE}"
+fi
+
+
+echo -e "\n---------------------------------------------------"
+echo "-- extracting boost's tarball"
+tar -xa -C ${BOOST_DIR} -f ${BOOST_FILE}
+
+# bootstrap bjam
+echo -e "\n---------------------------------------------------"
+echo "-- bootstrap bjam"
+cd ${BOOST_DIR}/boost_*
+./bootstrap.sh
+
+
+# Building bcp - boosts own filtering tool
+echo -e "\n---------------------------------------------------"
+echo "-- creating bcp"
+cd tools/bcp
+../../bjam --build-dir=${BOOST_BUILD_DIR}
+cd ../..
+cp $(ls ${BOOST_BUILD_DIR}/boost/*/tools/bcp/*/*/*/bcp) .
+
+
+# Building the required libraries
+echo -e "\n---------------------------------------------------"
+echo "-- running bjam"
+echo "using gcc : : ${MINGW_GPP} ;" > ${BOOST_CONF}
+./bjam \
+    -j5 \
+    --build-dir="${BOOST_BUILD_DIR}" \
+    --stagedir="${MINGWLIBS_DIR}" \
+    --user-config=${BOOST_CONF} \
+    --debug-building \
+    --layout="tagged" \
+    ${BOOST_LIBS_ARG} \
+    variant=release \
+    target-os=windows \
+    threadapi=win32 \
+    threading=multi \
+    link=static \
+    toolset=gcc \
+
+
+# Copying the headers to MinGW-libs
+echo -e "\n---------------------------------------------------"
+echo "-- copying headers"
+rm -Rf ${BOOST_BUILD_DIR}/filtered
+mkdir ${BOOST_BUILD_DIR}/filtered
+./bcp ${BOOST_HEADERS} ${BOOST_BUILD_DIR}/filtered
+cp -r ${BOOST_BUILD_DIR}/filtered/boost ${MINGWLIBS_DIR}include/
+
+
+# some config we need
+echo -e "\n---------------------------------------------------"
+echo "-- adjusting config/user.hpp"
+echo "#define BOOST_THREAD_USE_LIB" >> "${MINGWLIBS_DIR}include/boost/config/user.hpp"
+
+
+# fix names
+echo -e "\n---------------------------------------------------"
+echo "-- fix some library names"
+mv "${MINGWLIBS_DIR}lib/libboost_thread_win32-mt.a" "${MINGWLIBS_DIR}lib/libboost_thread-mt.a"
+
+
+# push to git repo
+echo -e "\n---------------------------------------------------"
+echo "-- git push"
+cd ${MINGWLIBS_DIR}
+BOOST_VERSION=$(grep "#define BOOST_VERSION " ./include/boost/version.hpp | awk '{print $3}')
+BOOST_VERSTR="$((BOOST_VERSION / 100000)).$((BOOST_VERSION / 100 % 1000)).$((BOOST_VERSION % 100))"
+git remote add cloud git@github.com:spring/mingwlibs.git
+git add --all
+git commit -m "boost update (boost: ${BOOST_VERSTR} gcc: ${GCC_VERSION})"
+git push cloud
+
+
+# cleanup
+echo -e "\n---------------------------------------------------"
+echo "-- cleanup"
+rm -rf ${BOOST_BUILD_DIR}
+rm -rf ${BOOST_DIR}
+rm -rf ${MINGWLIBS_DIR}
+
